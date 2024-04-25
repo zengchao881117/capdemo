91c255487bd896d845f523c519fabbdb4e971e9e
zengchao1117 <zengchao1117@163.com>
2024-04-25 15:29:10 +0800

    first commit

---
+++ app/book/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'ns/book/test/integration/FirstJourney',
+		'ns/book/test/integration/pages/BooksList',
+		'ns/book/test/integration/pages/BooksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, BooksList, BooksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('ns/book') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheBooksList: BooksList,
+					onTheBooksObjectPage: BooksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
